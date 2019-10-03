688c87e24ce8b3dee525a389528de3edc92d14b6
pu37 <45887287+pu37@users.noreply.github.com>
2019-10-02 22:32:16 -0700

    Update phase2-main.py

--- phase2-main.py
+++ phase2-main.py
@@ -48,11 +48,11 @@ def LBP(colln_imgs):
         for h in range(0, img_h-win_h+1, win_h):
             for w in range(0, img_w-win_w+1, win_w):
                 win = img[h:h+win_h, w:w+win_w]
-                (hist, _) = np.histogram((local_binary_pattern(win, 8, 1)).ravel(), bins=np.arange(0, 8 + 3), range=(0, 8 + 2))
+                (hist, _) = np.histogram((local_binary_pattern(win, 24, 8)).ravel(), bins=np.arange(0, 24 + 3), range=(0, 24 + 2))
                 hist = hist.astype("float")
                 hist /= (hist.sum() + 1e-7)
                 lbp.append(hist)
-        colln_lbp.append(hist)
+        colln_lbp.append(lbp)
     return colln_lbp
 
 
